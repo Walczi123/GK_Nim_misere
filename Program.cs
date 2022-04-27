5fb6a110baf861355d16bccc08c575c20641e535
szymczakf <filip.p.szymczak@gmail.com>
2022-04-27 17:43:57 +0200

    Optimal strategy

--- Program.cs
+++ Program.cs
@@ -2,12 +2,22 @@
 using Nim_misere.Game;
 using Nim_misere.Player;
 
-Console.WriteLine("\n\n");
-Console.WriteLine("Your game begins!\n");
+namespace Nim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Your game begins!\n");
 
-var state = new State() { Stacks = new List<int> { 3, 3, 3 } };
-var game = new NimMisereGame(new Man(), new MCTS(), state);
-game.Start();
+            var state = new State() { Stacks = new List<int> { 3, 3, 3 } };
+            //var game = new NimMisereGame(new Man(), new MCTS(), state);
+            var game = new NimMisereGame(new Man(), new Optimal(), state);
+            game.Start();
 
-Console.WriteLine("Bye");
-Console.ReadKey();
+            Console.WriteLine("Bye");
+            Console.ReadKey();
+        }
+    }
+}
