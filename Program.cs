fc61562ff7e36864223c66c8acbcaacc89ca3c99
szymczakf <filip.p.szymczak@gmail.com>
2022-04-27 17:58:50 +0200

    Revert "Optimal strategy"
    This reverts commit 5fb6a110baf861355d16bccc08c575c20641e535.
    
    Revert

--- Program.cs
+++ Program.cs
@@ -2,22 +2,12 @@
 using Nim_misere.Game;
 using Nim_misere.Player;
 
-namespace Nim
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("\n\n");
-            Console.WriteLine("Your game begins!\n");
+Console.WriteLine("\n\n");
+Console.WriteLine("Your game begins!\n");
 
-            var state = new State() { Stacks = new List<int> { 3, 3, 3 } };
-            //var game = new NimMisereGame(new Man(), new MCTS(), state);
-            var game = new NimMisereGame(new Man(), new Optimal(), state);
-            game.Start();
+var state = new State() { Stacks = new List<int> { 3, 3, 3 } };
+var game = new NimMisereGame(new Man(), new MCTS(), state);
+game.Start();
 
-            Console.WriteLine("Bye");
-            Console.ReadKey();
-        }
-    }
-}
+Console.WriteLine("Bye");
+Console.ReadKey();
