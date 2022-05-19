0cd40da08393849b2de885e1ba6e44b82e3769d2
martin <maartinmxxx@gmail.com>
2022-05-19 19:37:11 +0200

    Clean code

--- Utils/KayboardReader.cs
+++ Utils/KayboardReader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -45,6 +46,62 @@ namespace Nim_misere.Utils
                 {
                     return number;
                 }
+            }
+        }
+
+        public static T ReadOption<T>(string question, List<T> options) where T: IConvertible
+        {
+            T item;
+            while (true)
+            {
+                Console.WriteLine(question);
+                try
+                {
+                    item = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+                }
+                catch { Console.WriteLine("This is not a number. Choose again.\n"); continue; }
+
+                if (!options.Contains(item)) Console.WriteLine("This number is not one of the options. Choose again.\n");
+                else
+                {
+                    return item;
+                }
+            }
+        }
+
+        public static List<int> ReadStackSizes(int stacksAmount)
+        {
+            List<int> stackSizes = new List<int>();
+            while (true)
+            {
+                Console.WriteLine("How many elements should be on the stacks? /'E.g. 2,3,4 /'");
+                try
+                {
+                    stackSizes = new List<int>();
+                    var answer = Console.ReadLine();
+                    if (answer == null)
+                        throw new Exception("Empty answer.");
+                    var stacks = answer.Split(",");
+                    if (stacks.Length != stacksAmount)
+                        throw new Exception("Wrong amount of stacks.");
+                    foreach (var stack in stacks)
+                    {
+                        var pars = Int32.TryParse(stack, out var res);
+                        if (pars == false)
+                            throw new Exception("Not a number.");
+                        else
+                        {
+                            if (res > 0)
+                                stackSizes.Add(res);
+                            else
+                                throw new Exception("Negative number.");
+                        }
+                    }
+                }
+                catch { Console.WriteLine("This is not a valid option. Choose again.\n"); continue; }
+
+                if (stackSizes.Count <= 0) Console.WriteLine("It's too few. Choose again.\n");
+                else return stackSizes;
             }
         }
     }
