fbb1078a63bf733b5cde5d3b95825d5c41f66b07
martin <maartinmxxx@gmail.com>
2022-05-19 19:09:52 +0200

    utils

---
+++ Utils/KayboardReader.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nim_misere.Utils
+{
+    public static class KayboardReader
+    {
+        public static int ReadPositiveInteger(string question)
+        {
+            int number;
+            while (true)
+            {
+                Console.WriteLine(question);
+                try
+                {
+                    number = Convert.ToInt32(Console.ReadLine());
+                }
+                catch { Console.WriteLine("This is not a number. Choose again.\n"); continue; }
+
+                if (number <= 0) Console.WriteLine("The number has to be positive. Choose again.\n");
+                else
+                {
+                    return number;
+                }
+            }
+        }
+
+        public static int ReadNonnegativeInteger(string question)
+        {
+            int number;
+            while (true)
+            {
+                Console.WriteLine(question);
+                try
+                {
+                    number = Convert.ToInt32(Console.ReadLine());
+                }
+                catch { Console.WriteLine("This is not a number. Choose again.\n"); continue; }
+
+                if (number < 0) Console.WriteLine("The number has to be nonnegative. Choose again.\n");
+                else
+                {
+                    return number;
+                }
+            }
+        }
+    }
+}
